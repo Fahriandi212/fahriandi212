2f4a351b879f5b730d9371eff1f93a44932878da
fariandi212 <fariandi212@email.com>
2023-10-17 21:08:28 +0700

    pert2, menggunakan mvc, contoh 3

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -4,15 +4,13 @@ class Latihan1 extends CI_Controller
 {
     public function index()
     {
-        echo "<h1?> ...</h1>";
-        echo "Nama saya Fahriandi Azis 
-        Saya Tinggal di Permata Hijau Permai 
-        Hobi saya berenang" ;
+        echo "Selamat datang, selamat belajar web proramming";
     }
 
     public function penjumlahan($n1, $n2) {
         $this->load->model('Model_latihan1');
         $hasil = $this->Model_latihan1->jumlah($n1, $n2);
-        echo "Hasil Penjumlahan dari $n1 + $n2 adalah $hasil";
+
+        $this->load->view('view-latihan', compact('hasil', 'n1', 'n2'));
     }
 }
